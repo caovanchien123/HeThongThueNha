529f8eb063005632d675f2840ffcac021bbcc88a
caovanchien123 <caovanchien.tdc2018@mail.com>
2020-12-07 12:02:32 +0700

    Báo cáo

---
+++ app/src/main/java/com/example/hethongthuenha/Adapter/PhongTro.java
@@ -0,0 +1,31 @@
+package com.example.hethongthuenha.Adapter;
+
+public class PhongTro {
+    String roomName,roomPrice;
+
+    public PhongTro() {
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomPrice() {
+        return roomPrice;
+    }
+
+    public void setRoomPrice(String roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+
+    public PhongTro(String roomName, String roomPrice) {
+        this.roomName = roomName;
+        this.roomPrice = roomPrice;
+    }
+
+
+}
