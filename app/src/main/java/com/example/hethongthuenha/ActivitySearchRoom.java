5cac63f46ad5b30c1ff81829dbc0be1a1c31861b
ThanhHuy <phamthanhhuy332@gmail.com>
2020-12-09 16:35:55 +0700

    Giao diện tìm kiếm

---
+++ app/src/main/java/com/example/hethongthuenha/ActivitySearchRoom.java
@@ -0,0 +1,33 @@
+package com.example.hethongthuenha;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class ActivitySearchRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_room);
+    }
+
+}
