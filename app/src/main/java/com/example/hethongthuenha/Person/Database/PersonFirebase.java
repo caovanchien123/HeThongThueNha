e7f596ba712f79c5f6c90017c4c047ec15400d8a
ThanhHuy <phamthanhhuy332@gmail.com>
2021-01-13 21:58:30 +0700

    Gộp Sprint 3

---
+++ app/src/main/java/com/example/hethongthuenha/Person/Database/PersonFirebase.java
@@ -0,0 +1,7 @@
+package com.example.hethongthuenha.Person.Database;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class PersonFirebase {
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+}
