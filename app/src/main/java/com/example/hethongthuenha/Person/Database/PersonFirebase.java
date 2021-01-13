39d50dd8dab1723f9ca2f5528fb54cac681c9cf3
ThanhHuy <phamthanhhuy332@gmail.com>
2021-01-13 22:16:15 +0700

    Sprint 3 sua loi

--- app/src/main/java/com/example/hethongthuenha/Person/Database/PersonFirebase.java
+++
@@ -1,7 +0,0 @@
-package com.example.hethongthuenha.Person.Database;
-
-import com.google.firebase.firestore.FirebaseFirestore;
-
-public class PersonFirebase {
-    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
-}
